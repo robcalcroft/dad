d10d5870d898a908e0c77f074a22ee40dcebe257
robcalcroft <rob@calcroft.com>
2014-04-23 01:25:48 +0100

    first draft

---
+++ stuff.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+	// setTimeout(function() {
+		$('.header-hover-centre').animate({
+			left: "+=5%",
+			opacity: 0
+		},{
+			duration: 500,
+			complete: function() {
+				$(this).hide();
+				$('div.main-container').fadeIn('slow');
+			}
+		})
+	// },2000);
+	site.bindClick();
+})
+
+var site = {
+	bindClick : function() {
+		$('ul.main-nav li').click(function() {
+			var a = $(this).html();
+			$('html, body').animate({
+				scrollTop: $('#'+a).offset().top
+			}, 800);
+		});
+
+		$('.scrollTop').click(function() {
+			$('html, body').animate({
+				scrollTop: '0'
+			}, 500);
+		})
+	}
+}
